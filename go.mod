1a43d560ac4b3d1d7bb8d809fd6b8a511b1e0825
biny <15802080081@163.com>
2025-07-01 20:39:27 +0800

    7.1 将NewHTTPServer放到loong中，由loong生产返回，增加简单中间传入例子，controller半自动Register

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.4
 
 require (
 	github.com/emicklei/proto v1.14.2
+	github.com/go-kratos/kratos/v2 v2.8.4
 	github.com/spf13/cobra v1.9.1
 	golang.org/x/mod v0.25.0
 	golang.org/x/text v0.26.0
@@ -11,6 +12,16 @@ require (
 )
 
 require (
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/grpc v1.61.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -1,6 +1,12 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/http"
+)
 
 func Start() {
 	// wire注入如何处理？
@@ -12,4 +18,32 @@ func Start() {
 	// 启动 http
 	// 启动 prc
 	fmt.Println("Starting http rpc server...")
+}
+
+// server/server.go
+func NewHTTPServer(opts HTTPServerOptions) *http.Server {
+	var lOpts = []http.ServerOption{
+		http.Address(opts.Address),
+		http.Middleware(
+			recovery.Recovery(),
+		),
+		http.Middleware(opts.Middlewares...),
+	}
+	srv := http.NewServer(lOpts...)
+
+	for _, r := range opts.Registrars {
+		r.Register(srv)
+	}
+
+	return srv
+}
+
+type HTTPServerOptions struct {
+	Address     string
+	Middlewares []middleware.Middleware
+	Registrars  []HTTPServiceRegistrar
+}
+
+type HTTPServiceRegistrar interface {
+	Register(*http.Server)
 }
